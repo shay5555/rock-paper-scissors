031c3135d0cf8f9f72bac299fcea164b02c046cb
shay5555 <62674745+shay5555@users.noreply.github.com>
2022-12-22 18:10:38 +0200

    Write 'playRockPaperScissors'

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ function getComputerChoice () {
     }
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection) {
+        console.log(`Tie game! You both picked ${computerSelection}`);
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return ('You lost! paper beats rock');
+        } else if (computerSelection === 'scissors') {
+            return ('You won! Rock beats scissors');
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            return ('You lost! scissors beats paper');
+        } else if (computerSelection === 'rock') {
+            return ('You won! paper beats rock');
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            return ('You lost! rock beats scissors');
+        } else if (computerSelection === 'paper') {
+            return ('You won! scissors beats paper');
+        }
+    } else {
+        return('You entered an invalid value.');
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
