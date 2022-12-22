55b3de48949070ac4b831c3a20f4d85fc12211e0
shay5555 <62674745+shay5555@users.noreply.github.com>
2022-12-22 17:40:46 +0200

    Write getComputerChoice

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice () {
+    let computerChoice;
+    computerChoice = Math.floor(Math.random() * 3 + 1);
+    switch(computerChoice) {
+        case 1:
+            return ('rock');
+        case 2:
+            return ('paper');
+        case 3:
+            return ('scissors');
+    }
+}
+
