578e97917bd77a65a426951eaedf8f583b1c2213
shay5555 <62674745+shay5555@users.noreply.github.com>
2022-12-25 15:01:30 +0200

    Remove comment

--- script.js
+++ script.js
@@ -82,13 +82,3 @@ function playRound(playerSelection, computerSelection) {
         roundResult.textContent = '';
     }
 }
-
-//play the game with prompt 
-/* function game () {
-    for (let i=0; i<5; i++) {
-        const playerSelection = prompt("Write you choice; rock, paper or scissors: ", 'rock');
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
- */ 
