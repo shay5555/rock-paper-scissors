940502070d9863c946bedb1274f06de62bdf6fea
shay5555 <62674745+shay5555@users.noreply.github.com>
2024-04-07 16:58:30 +0300

    Create function 'playRound'

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function getComputerChoice() {
         return 'scissors';
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == 'rock'     && computerSelection == 'scissors' || 
+        playerSelection == 'paper'    && computerSelection == 'rock'     || 
+        playerSelection == 'scissors' && computerSelection == 'paper') {
+            console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+            return 'win';
+    } else if (playerSelection === computerSelection) {
+        console.log(`Draw - both picked ${playerSelection}.`);
+        return 'draw';
+    } else {
+        console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+        return 'loss';
+    }
+}
